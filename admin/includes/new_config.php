031a60be82fa75d85622adb9200c9ddb64faa637
zlatkom1991 <zlatkom1991@gmail.com>
2020-03-20 20:26:25 +0100

    new config

--- admin/includes/new_config.php
+++ admin/includes/new_config.php
@@ -1,6 +1,6 @@
 <?php
 
-define('DB_HOST','localhost');
+define('DB_HOST','169.254.0.2');
 define('DB_USER','zlatkom_admin');
 define('DB_PASS','Kambaradahija91');
 define('DB_NAME','zlatkom_portal');
